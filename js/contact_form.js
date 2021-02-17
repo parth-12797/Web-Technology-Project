b034de2a3d77b547d263904289b9392018f3a5ed
parth-12797 <78936647+parth-12797@users.noreply.github.com>
2021-02-17 18:42:24 -0500

    Update contact_form.js
    email_id validation added

--- js/contact_form.js
+++ js/contact_form.js
@@ -1,4 +1,15 @@
-$("#email").keyup(function(){
-  alert("parth")
-  /*$("input").css("background-color", "pink");*/
-});
+$(document).ready(function(){
+  $("#email").keyup(function(){
+    var regex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+     var emailaddressVal = $("#email").val();
+  if(!regex.test(emailaddressVal)) {
+  	//alert("parth");
+    $("#email_val").html("enter proper email id");
+  }else{
+
+$("#email_val").html("");
+     
+     }
+  });
+
+   });
