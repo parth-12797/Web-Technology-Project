1bb3d1ecb1e4897083a074be7a1555cf7d3436d8
asheshpatel65 <78936648+asheshpatel65@users.noreply.github.com>
2021-04-08 13:04:54 -0400

    Update contact_form.js

--- js/contact_form.js
+++ js/contact_form.js
@@ -1,68 +1,21 @@
-$(document).ready(function(){
-  $("#email").keyup(function(){
-    var regex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-     var emailaddressVal = $("#email").val();
-  if(!regex.test(emailaddressVal)) {
-  	//alert("parth");
-    $("#email_val").html("enter proper email id");
-  }else{
-
-$("#email_val").html("");
-     
-     }
-  });
-  
-  $("#phone").keyup(function(){
-      var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-      var phoneVal = $("#phone").val();
-  if(!phoneno.test(phoneVal)) {
-  	//alert("parth");
-    $("#phone_val").html("enter valid phone number");
-  }else{
-
-$("#phone_val").html("");
-     
-     }
-  });
-
-    $("#pwd").keyup(function(){
-  	if($("#pwd").val()=="")
-  	{
-  		$("#conf_pwd").val('');
-  		$("#CheckPasswordMatch").html("");
-  	}
-      var password = /^(?=.*[A-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@])(?!.*[iIoO])\S{6,12}$/;
-      var passwordval = $("#pwd").val();
-  if(!password.test(passwordval)) {
-  	//alert("parth");
-    $("#pass_val").html("Must contain at least one number and one uppercase and lowercase letter,special character and  at least 8 or more characters");
-  }
-  else{
-$("#pass_val").html("");
-     
-     }
-  });
-
-$("#conf_pwd").keyup(function(){
-        var password = $("#pwd").val();
-        var confirmPassword = $("#conf_pwd").val();
-        if (password != confirmPassword)
+function myFuction() {
+       var form = document.getElementById("form");
+       var email = document.getElementById("email").value;
+      var text = document.getElementById("text");
+       var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+        
+        if(email.match(pattern))
             {
-            	$("#CheckPasswordMatch").css({
-            	"color": "red",
-            });
-            $("#CheckPasswordMatch").html("Passwords does not match!");
+                form.classList.add("valid");
+                form.classList.remove("valid");
+                text.innerHTML = "valid email";
+                text.style.color ="green";
             }
-        else
-        {
-            $("#CheckPasswordMatch").html("Passwords match.");
-            $("#CheckPasswordMatch").css({
-            	"color": "green",
-            });
-            
-
-        }
-
-  });
-
-   });
+          else{
+              
+                form.classList.remove("valid");
+                form.classList.add("valid");
+                text.innerHTML = "invalid email";
+                text.style.color ="red";
+              }
+  }    
